154410615249fe2f5b659b1da7f3be7b56d28784
mohragk <sandervermeer1@gmail.com>
2018-02-15 20:53:14 +0100

    deviation leads to aliasing, so no, no

--- source/Oscillator.h
+++ source/Oscillator.h
@@ -124,7 +124,7 @@ public:
         {
             phase.set(0.0);
             rephase = true;
-            deviation = random.nextFloat() * 0.15;
+			deviation *= -1.0;
             
         }
         
